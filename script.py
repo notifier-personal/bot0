8d3564a56749923e449b93cc852db4fb508bd504
dharshanspn <86490616+dharshanspn@users.noreply.github.com>
2024-11-08 18:26:34 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import os
 
 
 x = 2  # Starting row index (inclusive) Row starts from 0 x=2 means Row 3
-y = 15  # Ending row index (exclusive) 15 Row pomints to 14 row, but since it starts from 0 it points to 15 row
+y = 30  # Ending row index (exclusive) 15 Row pomints to 14 row, but since it starts from 0 it points to 15 row
 
 
 # Access secrets from environment variables
